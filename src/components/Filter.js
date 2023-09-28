93d39db269e07f4a1b28fa0b97b23598148a90d0
Rishabh523 <parishabh8787@gmail.com>
2023-09-29 03:41:44 +0530

    ok

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -13,8 +13,12 @@ const Filter = (props) => {
         <div className='w-11/12 flex flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center'> 
           {
           filterData.map( (data) => (
-            <button className={'text-lg px-2 py-1 rouunded-md font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300
-            ${category===data.title ? "opacity-60 border-white" : "opacity-40 border-transparent"}'} 
+            <button className={`text-lg px-2 py-1 rounded-md font-medium 
+            text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300
+            ${category === data.title ? 
+              "opacity-60 border-white" : 
+              "opacity-40 border-transparent"}
+              `} 
             key={data.id}
             onClick={() => filterHandler(data.title)}>
             {data.title}
